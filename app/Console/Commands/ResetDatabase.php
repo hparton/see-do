09969abe733b89694d5993381396659baba1ddfa
hparton <hparton@hotmail.co.uk>
2017-08-20 17:07:46 +0000

    add db:reset command

---
+++ app/Console/Commands/ResetDatabase.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Schema;
+
+class ResetDatabase extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'db:reset';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Drop all database tables and rerun the migrations.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $tables = [];
+
+	DB::statement('SET FOREIGN_KEY_CHECKS=0');
+
+	foreach (DB::select('SHOW TABLES') as $k => $v) {
+		$tables[] = array_values((array)$v)[0];
+	}
+
+	foreach ($tables as $table) {
+		Schema::drop($table);
+		echo "Table " . $table . " has been dropped." . PHP_EOL;
+	}
+
+	$this->call('migrate');
+
+	if ($this->option('seed')) {
+		$this->call('db:seed');
+	}
+
+	$this->call('migrate');
+    }
+}
